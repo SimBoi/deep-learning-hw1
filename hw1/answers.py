61db6bd6ed4b4bf05e295930b4c82ecae2074ad0
Hamza Odeh <dev@simboi.com>
2024-02-08 23:09:21 +0200

    part 2 dry

--- hw1/answers.py
+++ hw1/answers.py
@@ -34,32 +34,23 @@ No, it's not justified. The test set was utilized during the training of the cla
 part2_q1 = r"""
 **Your answer:**
 
+The choice of $\Delta > 0$ in the SVM loss function $L(\mat{W})$ is arbitrary because any change in its value can be compensated by a proportional adjustment in the weight matrix $\mat{W}$. If we multiply $\Delta$ by a positive constant, we can also multiply the weight matrix $\mat{W}$ by the same constant. This results in all margin losses being scaled by the same factor, leaving the maximum value unchanged. This is because the outcome is not affected by scaling the weight matrix with any positive scalar.
 
 """
 
 part2_q2 = r"""
 **Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+The linear model is learning to identify subspaces of pixels where the projection of vectors onto these subspaces yields a high value for the correct class. Some classification errors can be observed in instances where the samples have a high projection onto the subspace of an incorrect class.
 
 """
 
 part2_q3 = r"""
 **Your answer:**
 
+1. The chosen learning rate appears to be optimal as the model exhibits good convergence. If the learning rate was too low, the model would either converge very slowly or not at all. Conversely, if the learning rate was too high, the model would either diverge or show fluctuations.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+2. The model seems to be slightly overfitting, as evidenced by its high accuracy and low loss on the training set compared to a slightly lower accuracy and slightly higher loss on the test set.
 
 """
 
